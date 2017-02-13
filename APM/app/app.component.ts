ee5add5fe50ca315d1dec053e1fa714618eb9247
Jason Robertson <comsys@comsys.solutions>
2017-02-13 11:05:40 +1100

    commit local

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
 
+// this imports the component function from Angular Core
+import {Component} from '@angular/core'; 
+
+// selector pm for priduct management - App name of the component any valid html can be used in the template
 @Component({
     selector: 'pm-app',
-    template: `
-        <h1>Angular2: Getting Started</h1>
-    `
+    template:'<div><h1>{{pageTitle}}</h1><div>My First Component in Angular 2 yee ha</div></div>'
 })
-export class AppComponent { }
+
+// creates the class export makes it public
+export class AppComponent {
+    pageTitle:string = 'Jasons Angular 2 Website Web App'
+}
--- APM/app/main.ts
+++ APM/app/main.ts
@@ -1,5 +1,7 @@
-// main entry point
+// main entry point - angular compile - compiles in browser dynamically
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
 
+// where loaded can find applications angular module to load it no extensions but in systemjs.config.js says its js and maps locations of dir's and files
+import { AppModule } from './app.module';
+// load angular module
 platformBrowserDynamic().bootstrapModule(AppModule);
