0b400650761b90399eba6e1527de8520f2df00bc
Jason Robertson <comsys@comsys.solutions>
2017-02-14 13:40:33 +1100

    commot all

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -5,7 +5,8 @@ import {Component} from '@angular/core';
 // selector pm for priduct management - App name of the component any valid html can be used in the template
 @Component({
     selector: 'pm-app',
-    template:'<div><h1>{{pageTitle}}</h1><div>My First Component in Angular 2 yee ha</div></div>'
+    template:`<div><h1>{{pageTitle}}</h1><div>My First Component in Angular 2 yee ha</div>
+              <pm-products></pm-products></div>`
 })
 
 // creates the class export makes it public
---
+++ APM/app/products/product-list.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from 'angular/core';
+
+// component decorator
+@Component({
+    selector: 'pm-products'; 
+    tempplateUrl : 'app/products/product-list.component.html' // relatice to index.html file 
+})
+
+// class
+export class ProductListCompmenmt {
+
+}
